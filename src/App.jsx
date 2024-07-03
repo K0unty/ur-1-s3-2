8b21e3ee55b811587e4a95abe84b5da6490806ab
K0unty <kountiapi@pm.me>
2024-07-03 07:05:52 +0530

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+function Header() {
+  return (
+    <header>
+      <img src="https://i.ibb.co/HB8T6tX/image.png" alt="Stylized atom" />
+      <h1>Weapon Essentials</h1>
+      <p>
+        WeaponReact concepts you will need for almost any app you are going to
+        build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="https://i.ibb.co/HB8T6tX/image.png" alt="Stylized atom" />
-        <h1>Weapon Essentials</h1>
-        <p>
-          WeaponReact concepts you will need for almost any app you are going to
-          build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
