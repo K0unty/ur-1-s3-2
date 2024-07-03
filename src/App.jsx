a80120b8f42a50e91adf0fe1d6df9ab59c476666
K0unty <kountiapi@pm.me>
2024-07-03 08:03:02 +0530

    JSX ex[ressopms

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
+// Dynamic Content array
+const reactDescriptions = [
+  "🍑BootySniff",
+  "👙PantySniff",
+  "🙆‍♀️ArmpitSniff",
+  "👃🍑InHerBum",
+  "👃😹NoseInHerPus",
+];
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+// Header Function which you defined
 function Header() {
   return (
     <header>
       <img src="https://i.ibb.co/HB8T6tX/image.png" alt="Stylized atom" />
       <h1>Weapon Essentials</h1>
       <p>
-        WeaponReact concepts you will need for almost any app you are going to
-        build!
+        <span className="dynamic-text">
+          {reactDescriptions[getRandomInt(4)]}
+        </span>
+        concepts you will need for almost any app you are going to build!
       </p>
     </header>
   );
 }
 
+// Main App entry point
 function App() {
   return (
     <div>
