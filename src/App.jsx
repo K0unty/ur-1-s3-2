93120619dfa849ebaab42cf3a36f46198381d48c
K0unty <kountiapi@pm.me>
2024-07-03 06:12:05 +0530

    fixVS.txt

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
         <img src="https://i.ibb.co/HB8T6tX/image.png" alt="Stylized atom" />
         <h1>Weapon Essentials</h1>
         <p>
-          Fundamental WeaponReact concepts you will need for almost any app you
-          are going to build!
+          WeaponReact concepts you will need for almost any app you are going to
+          build!
         </p>
       </header>
       <main>
