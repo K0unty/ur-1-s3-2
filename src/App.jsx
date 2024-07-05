f9a7b00f0da74b3cfa3b5346d1338ae0efa3ba1e
K0unty <kountiapi@pm.me>
2024-07-05 23:41:00 +0530

    move files and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,6 @@
 // Importing images
-// import headImg1 from "https://i.ibb.co/HB8T6tX/image.png";
-import headImg2 from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data.js";
-
-// Dynamic Content array
-const reactDescriptions = [
-  "🍑BootySniff",
-  "👙PantySniff",
-  "🙆‍♀️ArmpitSniff",
-  "👃🍑InHerBum",
-  "👃😹NoseInHerPus",
-];
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-// Header Function which you defined
-function Header() {
-  const firstWord = reactDescriptions[getRandomInt(4)];
-
-  return (
-    <header>
-      <img src="https://i.ibb.co/HB8T6tX/image.png" alt="boty" />
-      <img src={headImg2} alt="Stylized atom" />
-      <h1>Weapon Essentials</h1>
-      <p>
-        <span className="dynamic-text">{firstWord}</span>
-        concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
+import Header from "./components/Header.jsx";
 
 // Props test
 function CoreConcept({ image, title, description }) {
